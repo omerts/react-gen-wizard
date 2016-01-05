974c5d0273c7e4d19020628a816f4de3abe11a75
Omer Spalter <omers@nsogroup.com>
2016-01-05 17:59:01 +0200

    initialdata example fix

--- example/main.js
+++ example/main.js
@@ -9,7 +9,7 @@ let testdata = [{name: 'Step 1', component: TestComponent},
                 {name: 'Step 2', component: TestComponent},
                 {name: 'Step 3', component: TestComponent}];
 
-ReactDOM.render(<Wizard components={testdata} data={{step: 1}} onFinish={() => {location.reload();}} />,
+ReactDOM.render(<Wizard components={testdata} initialData={{step: 1}} onFinish={() => {location.reload();}} />,
                  document.getElementById('example'), () => {
                    console.log('rendered');
                  });
